ffafff5bd63087849480ba29886478e1d65810b0
BhanuPrasanthReddyVelpula <prasanthvelpula766@gmail.com>
2026-03-04 00:30:08 +0530

    Fix Cors for Vercel11

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,27 +2,31 @@ import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
 export const protect = async (req, res, next) => {
-  let token;
+  try {
+    let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
       token = req.headers.authorization.split(" ")[1];
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.id).select("-password");
 
-      next();
-    } catch (error) {
-      return res.status(401).json({ message: "Not authorized" });
-    }
-  }
+      if (!req.user) {
+        return res.status(401).json({ message: "User not found" });
+      }
 
-  if (!token) {
+      return next();
+    }
+
     return res.status(401).json({ message: "No token" });
+
+  } catch (error) {
+    console.error("PROTECT ERROR:", error);
+    return res.status(401).json({ message: "Not authorized" });
   }
 };
 
